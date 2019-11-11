45261cd6bd035068d1427328f9dabeac6fdce16d
Craig Sullivan <hi@craigs.io>
2019-11-12 09:13:32 +1100

    Initial tests (#5)

---
+++ components/SelectArticle/__tests__/index.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import { Article } from '../../index'
+import { SelectArticle } from '../index'
+
+import article from './data/article'
+import useRouter from './data/useRouter'
+
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => article }))
+jest.mock('next/router', () => ({ useRouter: () => useRouter }))
+
+it('renders an article', async () => {
+  const renderer = TestRenderer.create(<SelectArticle slug="work" />)
+  const { root } = renderer
+
+  expect(root.findByType(Article)).not.toBeNull()
+})
--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testMatch: [
-    '<rootDir>/components/**/__tests__/**/*.{ts,tsx}',
-    '<rootDir>/lib/**/__tests__/**/*.{ts,tsx}',
-    '<rootDir>/pages/**/__tests__/**/*.{ts,tsx}'
+    '<rootDir>/components/**/__tests__/*.{ts,tsx}',
+    '<rootDir>/lib/**/__tests__/*.{ts,tsx}',
+    '<rootDir>/pages/**/__tests__/*.{ts,tsx}'
   ],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy'
   },
   globals: {
     'ts-jest': {
+      diagnostics: false,
       tsConfig: 'jest.tsconfig.json'
     }
   }
