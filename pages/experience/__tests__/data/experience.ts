dfc9787fd5cc1bf1cf2facface58a1dcb0230cd9
craigs <hi@craigs.io>
2019-11-13 22:35:16 +1100

    Add tests for /experience page

---
+++ pages/experience/__tests__/data/experience.ts
@@ -0,0 +1,10 @@
+export default {
+  data: {
+    article: {
+      __typename: 'Article',
+      body: 'Information will be posted shortly',
+      id: 'ck2rfqzjzpps009989sepp5gs',
+      title: 'Experience'
+    }
+  }
+}
---
+++ pages/experience/__tests__/index.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import { SelectArticle } from '../../../components'
+import Page from '../index'
+import experience from './data/experience'
+
+const useRouter = {
+  asPath: {
+    startsWith: () => false
+  }
+}
+
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => experience }))
+jest.mock('next/router', () => ({ useRouter: () => useRouter }))
+
+it('renders an article', async () => {
+  const renderer = TestRenderer.create(<Page />)
+  const { root } = renderer
+
+  expect(root.findByType(SelectArticle)).not.toBeNull()
+})
