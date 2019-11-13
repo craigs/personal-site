492610389285af899fb61a0fb9eef5539ecc6675
craigs <hi@craigs.io>
2019-11-13 22:38:55 +1100

    Add test for /articles page

---
+++ pages/articles/__tests__/index.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import { Articles } from '../../../components'
+import Page from '../index'
+import articles from './data/articles'
+
+const useRouter = {
+  asPath: {
+    startsWith: () => false
+  }
+}
+
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => articles }))
+jest.mock('next/router', () => ({ useRouter: () => useRouter }))
+
+it('renders an article', async () => {
+  const renderer = TestRenderer.create(<Page />)
+  const { root } = renderer
+
+  expect(root.findByType(Articles)).not.toBeNull()
+})
