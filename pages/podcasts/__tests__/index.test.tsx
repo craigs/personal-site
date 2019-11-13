0d3023120e1b81530867da5056f27e75036df56a
craigs <hi@craigs.io>
2019-11-13 22:26:40 +1100

    Add test for /podcasts page

--- pages/podcasts/__tests__/index.test.tsx
+++ pages/podcasts/__tests__/index.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import TestRenderer from 'react-test-renderer'
-import { Books } from '../../../components'
+import { Podcasts } from '../../../components'
 import Page from '../index'
-import books from './data/books'
+import podcasts from './data/podcasts'
 
 const useRouter = {
   asPath: {
@@ -10,12 +10,12 @@ const useRouter = {
   }
 }
 
-jest.mock('@apollo/react-hooks', () => ({ useQuery: () => books }))
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => podcasts }))
 jest.mock('next/router', () => ({ useRouter: () => useRouter }))
 
 it('renders an article', async () => {
   const renderer = TestRenderer.create(<Page />)
   const { root } = renderer
 
-  expect(root.findByType(Books)).not.toBeNull()
+  expect(root.findByType(Podcasts)).not.toBeNull()
 })
