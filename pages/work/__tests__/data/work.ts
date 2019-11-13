320009129532c0f81567bc9d4421a83bbfe59455
craigs <hi@craigs.io>
2019-11-13 22:31:42 +1100

    Add test for /work page

---
+++ pages/work/__tests__/data/work.ts
@@ -0,0 +1,10 @@
+export default {
+  data: {
+    article: {
+      __typename: 'Article',
+      body: 'Information will be posted shortly',
+      id: 'ck2rfq8xipppk0998km97f0yt',
+      title: 'Work'
+    }
+  }
+}
