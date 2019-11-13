bd16cb51c6c2e2b573c84af2a878a3b866999de5
craigs <hi@craigs.io>
2019-11-13 21:42:29 +1100

    Add Books test

---
+++ fixtures/book.ts
@@ -0,0 +1,10 @@
+export default {
+  author: 'Craig',
+  id: 1,
+  image: {
+    url: 'http://localhost/cover-image.jpg'
+  },
+  link: 'http://localhost/',
+  subtitle: 'A book about me',
+  title: 'My book'
+}
