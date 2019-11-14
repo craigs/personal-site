c741cbbaf64e2042d7eac2a85c1e1c023bd9fb15
craigs <hi@craigs.io>
2019-11-14 21:45:45 +1100

    More very rough mobile sizings

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown/with-html'
 
-export const Article = ({ title, body }) => (
-  <>
+import Styles from './index.css'
+
+export const Article = ({title, body}) => (
+  <div className={Styles.container}>
     <h1>{title}</h1>
     <ReactMarkdown escapeHtml={false} source={body} />
-  </>
+  </div>
 )
