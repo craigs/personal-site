60e6a5c5762c5c55965825d721a244449bf6fedb
craigs <hi@craigs.io>
2019-11-20 19:52:48 +1100

    Add swipeable for menu

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,43 +1,59 @@
 import Head from 'next/head'
-import React from 'react'
+import React, {useState} from 'react'
+import {Swipeable} from 'react-swipeable'
 import Favicon from './Favicon'
 import Sidebar from './Sidebar'
 
 import '../../styles/index.css'
 import Styles from './index.css'
 
-export const Layout = ({children}) => (
-  <>
-    <Head>
-      <title>craigs.io | Welcome</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <Favicon />
-    </Head>
+export const Layout = ({children}) => {
+  const [open, setOpen] = useState(false)
 
-    <div className={Styles.toggle}>
-      <label htmlFor="toggler">
-        <img src="/hamburger.svg" />
-      </label>
-    </div>
+  const config = {
+    onSwipedLeft: () => setOpen(false),
+    onSwipedRight: () => setOpen(true),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true
+  }
 
-    <input
-      name="toggler"
-      id="toggler"
-      className={Styles.toggler}
-      type="checkbox"
-    />
+  return (
+    <>
+      <Head>
+        <title>craigs.io | Welcome</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <Favicon />
+      </Head>
 
-    <div className={Styles.container}>
-      <Sidebar />
-
-      <div className={Styles.content}>
-        {children}
-
-        <div className={Styles.credits}>
-          <img src="https://graphcms.com/static/powered_by_graphcms-f67a6db8eb29a7e1b977c6b564120e34.svg" />
+      <Swipeable {...config}>
+        <div className={Styles.toggle}>
+          <label htmlFor="toggler">
+            <img src="/hamburger.svg" />
+          </label>
         </div>
-      </div>
-    </div>
-  </>
-)
+
+        <input
+          id="toggler"
+          checked={open}
+          className={Styles.toggler}
+          name="toggler"
+          onChange={() => setOpen(!open)}
+          type="checkbox"
+        />
+
+        <div className={Styles.container}>
+          <Sidebar />
+
+          <div className={Styles.content}>
+            {children}
+
+            <div className={Styles.credits}>
+              <img src="https://graphcms.com/static/powered_by_graphcms-f67a6db8eb29a7e1b977c6b564120e34.svg" />
+            </div>
+          </div>
+        </div>
+      </Swipeable>
+    </>
+  )
+}
