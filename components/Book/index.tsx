10eaffc5828a6cc95fd62b9c3fb999eee7af7c7f
craigs <hi@craigs.io>
2019-11-20 20:40:49 +1100

    Mobile refinements for Book

--- components/Book/index.tsx
+++ components/Book/index.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 import Styles from './index.css'
 
-export const Book = ({ link, image, title, subtitle, author }) => (
+export const Book = ({link, image, title, subtitle, author}) => (
   <div className={Styles.book}>
-    <a href={link}>
+    <a href={link} className={Styles.link}>
       <img src={image.url} />
-      <h3>{title}</h3>
-      {subtitle && <h4>{subtitle}</h4>}
-      <p>{author}</p>
+      <div>
+        <h3>{title}</h3>
+        {subtitle && <h4>{subtitle}</h4>}
+        <p>{author}</p>
+      </div>
     </a>
   </div>
 )
