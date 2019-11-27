ea520c201298ee5d6216014cf4ac64529f3f2ef7
craigs <hi@craigs.io>
2019-11-27 23:34:51 +1100

    Directly return component

--- components/Podcast/index.tsx
+++ components/Podcast/index.tsx
@@ -1,24 +1,22 @@
-import React from 'react'
 import GraphImg from 'graphcms-image'
+import React from 'react'
 
 import Styles from './index.css'
 
-export const Podcast = ({ link, image, title, author }) => {
-  return (
-    <a href={link} className={Styles.podcast}>
-      <GraphImg
-        backgroundColor={false}
-        className={Styles.image}
-        fit="scale"
-        image={image}
-        maxWidth={66}
-        withWebp={true}
-      />
+export const Podcast = ({ link, image, title, author }) => (
+  <a href={link} className={Styles.podcast}>
+    <GraphImg
+      backgroundColor={false}
+      className={Styles.image}
+      fit="scale"
+      image={image}
+      maxWidth={66}
+      withWebp={true}
+    />
 
-      <div>
-        <h3>{title}</h3>
-        <p>{author}</p>
-      </div>
-    </a>
-  )
-}
+    <div>
+      <h3>{title}</h3>
+      <p>{author}</p>
+    </div>
+  </a>
+)
