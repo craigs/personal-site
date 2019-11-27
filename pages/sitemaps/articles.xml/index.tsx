1e5b1a6bf70b88e6c2e50fc27d9d0a444ef03569
craigs <hi@craigs.io>
2019-11-27 21:38:01 +1100

    Move logic to the safety of the page

--- pages/sitemaps/articles.xml/index.tsx
+++ pages/sitemaps/articles.xml/index.tsx
@@ -1,6 +1,22 @@
+import { IncomingMessage } from 'http'
 import { ArticlesQuery } from '../../../graphql/ArticlesQuery'
 import { Api } from '../../../lib/Api'
-import { generate } from './generate'
+import { getUri } from '../../../lib/getUri'
+
+export const generate = (req: IncomingMessage, articles: any) => {
+  const uri = getUri(req)
+
+  const urls = articles.map(
+    article => `<url>
+        <loc>${uri}/articles/${article.slug}</loc>
+      </url>`
+  )
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls}
+    </urlset>`
+}
 
 // tslint:disable-next-line: no-empty
 const Page = () => {}
--- pages/sitemaps/others.xml/index.tsx
+++ pages/sitemaps/others.xml/index.tsx
@@ -1,4 +1,27 @@
-import { generate } from './generate'
+import { IncomingMessage } from 'http'
+import { getUri } from '../../../lib/getUri'
+
+export const generate = (req: IncomingMessage) => {
+  const uri = getUri(req)
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${uri}/bio</loc>
+      </url>
+      <url>
+        <loc>${uri}/books</loc>
+      </url>
+      <url>
+        <loc>${uri}/podcasts</loc>
+      </url>
+      <url>
+        <loc>${uri}/work</loc>
+      </url>
+    </urlset>`
+
+  return sitemap
+}
 
 // tslint:disable-next-line: no-empty
 const Page = () => {}
