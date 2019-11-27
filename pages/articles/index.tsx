fbe84b2883fa650bc9cbc2dd207219a118945d0e
craigs <hi@craigs.io>
2019-11-28 00:00:17 +1100

    Remove spinner from articles

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,29 +1,29 @@
 import { useQuery } from '@apollo/react-hooks'
 import Head from 'next/head'
 import React from 'react'
-import { Articles, Layout, Loading } from '../../components'
+import { Articles, Layout } from '../../components'
 import { ArticlesQuery } from '../../graphql/ArticlesQuery'
 import { Api } from '../../lib/Api'
 
-const Page = () => {
+const Page = ({ articles }) => (
+  <Layout>
+    <Head>
+      <title>Articles</title>
+    </Head>
+
+    <Articles collection={articles} />
+  </Layout>
+)
+
+Page.getInitialProps = async () => {
   const category = 'article'
-  const options = { client: Api, variables: { category } }
-  const { loading, error, data } = useQuery(ArticlesQuery, options)
 
-  if (loading) return <Loading />
-  if (error) return <div>Error :</div>
+  const { data } = await Api.query({
+    query: ArticlesQuery,
+    variables: { category }
+  })
 
-  const { articles } = data
-
-  return (
-    <Layout>
-      <Head>
-        <title>Articles</title>
-      </Head>
-
-      <Articles collection={articles} />
-    </Layout>
-  )
+  return { ...data }
 }
 
 export default Page
