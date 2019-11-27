9a7d4fffc5dde23d076fe827939aaabf0ff9c479
craigs <hi@craigs.io>
2019-11-27 23:08:33 +1100

    Speed up index page

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -1,4 +1,3 @@
-import { useQuery } from '@apollo/react-hooks'
 import Head from 'next/head'
 import React from 'react'
 import {
@@ -6,7 +5,6 @@ import {
   Books,
   ButtonLink,
   Layout,
-  Loading,
   Podcasts,
   Subscribe
 } from '../../components'
@@ -15,49 +13,43 @@ import { Api } from '../../lib/Api'
 
 import Styles from './index.css'
 
-const Page = () => {
-  const options = { client: Api }
-  const { loading, error, data } = useQuery(HomeQuery, options)
+const Page = ({ articles, books, podcasts }) => (
+  <Layout>
+    <Head>
+      <title>craigs.io - welcome</title>
+    </Head>
 
-  if (loading) return <Loading />
-  if (error) return <div>Error :</div>
+    <Articles collection={articles} />
 
-  const { articles, books, podcasts } = data
+    <div className={Styles.section}>
+      <hr />
+      <h1>Books</h1>
+      <p>Recent books I have listened to and have gained insights from</p>
 
-  return (
-    <Layout>
-      <Head>
-        <title>craigs.io - welcome</title>
-      </Head>
+      <Books collection={books} />
+      <ButtonLink label="more books" as="/books" link="/books" />
+    </div>
 
-      <Articles collection={articles} />
+    <div className={Styles.section}>
+      <hr />
+      <h1>Podcasts</h1>
+      <p>
+        The right selection of Podcasts is just the food a creative mind craves
+      </p>
 
-      <div className={Styles.section}>
-        <hr />
-        <h1>Books</h1>
-        <p>Recent books I have listened to and have gained insights from</p>
-        <Books collection={books} />
-        <ButtonLink label="more books" as="/books" link="/books" />
-      </div>
+      <Podcasts collection={podcasts} />
+      <ButtonLink label="more podcasts" as="/podcasts" link="/podcasts" />
+    </div>
 
-      <div className={Styles.section}>
-        <hr />
-        <h1>Podcasts</h1>
-        <p>
-          The right selection of Podcasts is just the food a creative mind
-          craves
-        </p>
+    <div className={Styles.section}>
+      <Subscribe />
+    </div>
+  </Layout>
+)
 
-        <Podcasts collection={podcasts} />
-
-        <ButtonLink label="more podcasts" as="/podcasts" link="/podcasts" />
-      </div>
-
-      <div className={Styles.section}>
-        <Subscribe />
-      </div>
-    </Layout>
-  )
+Page.getInitialProps = async ({ req, res }) => {
+  const { data } = await Api.query({ query: HomeQuery })
+  return { ...data }
 }
 
 export default Page
