bbe03819bd1e19885bf804ac6082d66cada026e8
craigs <hi@craigs.io>
2019-11-29 16:38:12 +1100

    Graphcms logo has been removed.
    References to the requirement seem to have gone away.

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head'
-import React, {useState} from 'react'
-import {Swipeable} from 'react-swipeable'
+import React, { useState } from 'react'
+import { Swipeable } from 'react-swipeable'
 import Favicon from './Favicon'
 import Sidebar from './Sidebar'
 
 import '../../styles/index.css'
 import Styles from './index.css'
 
-export const Layout = ({children}) => {
+export const Layout = ({ children }) => {
   const [open, setOpen] = useState(false)
 
   const config = {
@@ -49,13 +49,7 @@ export const Layout = ({children}) => {
         <div className={Styles.container}>
           <Sidebar />
 
-          <div className={Styles.content}>
-            {children}
-
-            <div className={Styles.credits}>
-              <img src="https://graphcms.com/static/powered_by_graphcms-f67a6db8eb29a7e1b977c6b564120e34.svg" />
-            </div>
-          </div>
+          <div className={Styles.content}>{children}</div>
         </div>
       </Swipeable>
     </>
