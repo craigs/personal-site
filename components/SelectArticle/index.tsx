8768beaefb4a15de95501352a9cd1a82df219ef9
craigs <hi@craigs.io>
2020-02-08 19:57:25 +1100

    Strip html from meta description

--- components/SelectArticle/index.tsx
+++ components/SelectArticle/index.tsx
@@ -1,8 +1,15 @@
 import Head from 'next/head'
 import React from 'react'
 import removeMd from 'remove-markdown'
+import sanitizeHtml from 'sanitize-html'
 import { Article, Layout } from '../../components'
 import { NotFound } from '../../lib/NotFound'
+
+const sanitize = (description: string): string => {
+  const stripped = sanitizeHtml(description)
+
+  return removeMd(stripped)
+}
 
 export const SelectArticle = (props: any) => {
   const article = props.article || NotFound
@@ -13,7 +20,7 @@ export const SelectArticle = (props: any) => {
         <title>{article.title}</title>
         <meta property="og:type" content="article" />
         <meta property="og:title" content={article.title} />
-        <meta property="og:description" content={removeMd(article.body)} />
+        <meta property="og:description" content={sanitize(article.body)} />
       </Head>
 
       <Article {...article} />
